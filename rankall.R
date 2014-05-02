e13fbb7aacaa9888ec784780e33e413266177a8b
Kyle Pace <kylepace03@gmail.com>
2014-05-01 20:41:49 -0400

    Rank all using lapply and split, for shits and giggles.

--- rankall.R
+++ rankall.R
@@ -1,14 +1,10 @@
 getRanked <- function(hosp, state, num) {
-	hosp <- subset(hosp, State == state)
 	ordered <- hosp[with(hosp, order(hosp[, 3], hosp[, 1], na.last = NA)), ]
-	if (nrow(ordered) == 0)
-		stop("invalid state")
-	
 	if (num == "worst") {
 		ordered[nrow(ordered), c(1, 2)]	
 	} else if (is.numeric(num)) {
 		if (nrow(ordered) < as.numeric(num)) {
-			data.frame(Hospital.Name = c(NA), State = c(state))
+			data.frame(hospital = c(NA), state = c(ordered(1, 2)))
 		} else {
 			ordered[as.numeric(num), c(1, 2)]
 		}
@@ -19,15 +15,9 @@ getRanked <- function(hosp, state, num) {
 
 rankall <- function(outcome, num = "best") {
 	outcomes <- cleanData(outcome)
+	colnames(outcomes) <- c("hospital", "state", "measure")
 	outcomes[, c(3)] <- as.numeric(outcomes[, c(3)])
-	for (state in unique(outcomes$State)) {
-		row <- getRanked(outcomes, state, num)
-		if (exists("acc")) {
-			acc <- rbind(acc, row)
-		} else {
-			acc <- row	
-		}
-	}
-	colnames(acc) <- c("hospital", "state")
-	arrange(acc, state)
+	outcomes <- arrange(outcomes, state)
+	output <- lapply(split(outcomes, outcomes$state), getRanked, num = num)
+	do.call(rbind.data.frame, output)
 }
